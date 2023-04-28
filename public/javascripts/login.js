87e29717a280f9b2fa6ef3343ac1ad287933a900
DuarteCavaleiro <99983370+DuarteCavaleiro@users.noreply.github.com>
2023-04-28 20:17:51 +0100

    test html

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,6 +1,7 @@
 async function login() {
     let msgDOM = document.getElementById("msg");
     msgDOM.textContent = "";
+    
     console.log(window.location);
     try {
         let name = document.getElementById("name").value;
@@ -8,10 +9,13 @@ async function login() {
         let result = await requestLogin(name,pass);
         if (result.err) {
             msgDOM.textContent = "An error occurred";
+            msgDOM.style.display='block';
         } else if (!result.successful) {
-            msgDOM.textContent = "Wrong username or password";    
+            msgDOM.textContent = "Wrong username or password";  
+            msgDOM.style.display='block';  
         } else {
-            msgDOM.textContent = "Login successful!";    
+            msgDOM.textContent = "Login successful!";  
+            msgDOM.style.display='block';  
             window.location.pathname = "/home.html"
         }
     } catch (err) {
